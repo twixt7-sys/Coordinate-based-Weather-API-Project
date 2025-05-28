e87b96ff9beb202325f002fff0fd765db68cad08
bloo <nexushellstein@gmail.com>
2025-05-28 19:46:52 +0800

    changed backend

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,10 +1,13 @@
 async function searchWeather() {
-	const city = document.getElementById('city').value;
+	const latitude = document.getElementById('latitude').value;
+	const longitude = document.getElementById('longitude').value;
 	const res = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=35&longitude=139&current_weather=true`);
 	const data = await res.json();
 	const temp = data.current_weather.temperature;
+	const wspeed = data.current_weather.windspeed;
 
-	document.getElementById('result').innerHTML = `Temperature in ${city}: ${temp}°C`;
+	document.getElementById('temperature').innerHTML = `Temperature in (${latitude}, ${longitude}): ${temp}°C`;
+	document.getElementById('windspeed').innerHTML = `Windspeed in (${latitude}, ${longitude}): ${temp}°C`;
 
 	// Save to DB
 	await fetch('../backend/save_weather.php', {
